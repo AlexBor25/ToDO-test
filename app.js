0acc8df77a4adb14eed4d397fb5bb57e21d8b54e
AlexBor <leo74710@gmail.com>
2020-09-20 22:25:24 +0300

    add save theme in localstorage

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ const tasks = [
       '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
     },
   };
-  let lastSelectedTheme = 'default';
+  let lastSelectedTheme = localStorage.getItem('app_theme') || 'default';
 
   // Переменные
   const listContainer = document.querySelector('.tasks-list-section .list-group'),
@@ -114,6 +114,7 @@ const tasks = [
 
 
   // События
+  setTheme(lastSelectedTheme);
   renderAllTaks(objOfTasks);
   form.addEventListener('submit', onFormSubmit);
   listContainer.addEventListener('click', onDeleteHandler);
@@ -215,6 +216,7 @@ function onThemeSelect(e){
   };
   setTheme(selectedTheme);
   lastSelectedTheme = selectedTheme;
+  localStorage.setItem('app_theme', selectedTheme);
 }
 
 function setTheme(name) {
